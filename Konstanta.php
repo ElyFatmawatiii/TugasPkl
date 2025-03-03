2d4af1dd18109d47d926c257b4de6735ae6b0d82
ElyFatmawatiii <138742104+ElyFatmawatiii@users.noreply.github.com>
2025-03-03 16:46:42 +0700

    Add files via upload

---
+++ Konstanta.php
@@ -0,0 +1,5 @@
+<?php
+echo "<h3>Konstanta</h3>";
+define("PRODI", "Manajemen Informatika");
+echo "Prodi: " . PRODI . "<br>";
+?>
---
+++ Tipe_Data.php
@@ -0,0 +1,17 @@
+<?php 
+// Integer
+echo "<h3>Tipe Data Integer</h3>";
+$angka = 100;
+echo "Angka: $angka<br>";
+
+// String 
+echo "<h3>Tipe Data String</h3>";
+$teks = "Hello, Saya Ely";
+echo "Teks: $teks<br>";
+
+// Float
+echo "<h3>Tipe Data Float</h3>";
+$desimal = 3.53;
+echo "Desimal: $desimal<br>";
+
+?>
